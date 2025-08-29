38388861a96588d703c5c02b90f4300d1c103bf8
J0aoD3v <jcmichelato8@gmail.com>
2025-08-29 20:49:40 -0300

    ok

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,17 +6,20 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="bg-gray-800 text-gray-300 py-10 text-center">
-      <div className="max-w-5xl mx-auto px-4 flex flex-col md:flex-row items-center justify-between">
-        <div className="mb-6 md:mb-0">
-          <p className="mt-4 text-sm">
+    <footer className="footer">
+      <div className="footer-content">
+        <div>
+          <p className="mt-4 text-sm" style={{ fontSize: "1.6rem" }}>
             © {currentYear} DRONALIZADO. Todos os direitos reservados.
           </p>
-        </div>
-        <div className="text-sm space-y-1">
-          <p>Bandeirantes - PR e região</p>
-          <p>(43) 99181-9073</p>
-          <p>belchior.julia12@gmail.com</p>
+          <div
+            className="footer-contact text-sm space-y-1"
+            style={{ marginTop: "0.5rem" }}
+          >
+            <p>Bandeirantes - PR e região</p>
+            <p>(43) 99181-9073</p>
+            <p>belchior.julia12@gmail.com</p>
+          </div>
         </div>
       </div>
     </footer>
