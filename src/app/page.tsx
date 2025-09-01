92a0f9cd66a9f2e8586aa23b4fed12f31307c5aa
J0aoD3v <jcmichelato8@gmail.com>
2025-08-31 21:26:53 -0300

    ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 // src/app/page.tsx
+"use client";
 import React, { useState } from "react";
 import HeroSection from "@/components/HeroSection";
 import ProblemSection from "@/components/ProblemSection";
